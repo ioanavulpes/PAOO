7c16826df80e60a3480aabc3dbf667d404da7fef
ioanavulpes <ioana.vulpes@student.upt.ro>
2025-11-18 20:34:59 +0200

    modificari

--- wardrobe/src/main.cpp
+++ wardrobe/src/main.cpp
@@ -10,20 +10,20 @@ Wardrobe makeWardrobe() {
 }
 
 int main() {
-    cout << "== Construct normal ==\n";
+    cout << " Construct normal \n";
     Wardrobe w1;
     w1.add(ClothingItem("Hanorac", "L", 189.99));
     w1.add(ClothingItem("Geaca", "M", 299.99));
     w1.print();
 
-    cout << "\n== Copy construct ==\n";
+    cout << "\n Copy construct \n";
     Wardrobe w2 = w1; 
     w2.print();
 
-    cout << "\n== Move construct ==\n";
+    cout << "\n Move construct \n";
     Wardrobe w3 = makeWardrobe(); 
     w3.print();
 
-    cout << "\n== End ==\n";
+    cout << "\n End \n";
     return 0;
 }
